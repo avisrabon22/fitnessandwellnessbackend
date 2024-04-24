6f363927e4f265ffe505146f51af0ffe877d95d7
avisrabon22 <avisrabon22@gmail.com>
2024-04-25 01:34:29 +0530

    first commit

---
+++ src/test/java/com/avijit/fitnessandwellnessbackend/FitnessandwellnessbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avijit.fitnessandwellnessbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessandwellnessbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
