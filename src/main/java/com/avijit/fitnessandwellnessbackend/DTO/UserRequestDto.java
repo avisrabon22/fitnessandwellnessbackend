d9a8cee21c367d5448d2a086fc92f6f3d73debdd
avisrabon22 <avisrabon22@gmail.com>
2024-04-25 07:17:19 +0530

    update on all module

---
+++ src/main/java/com/avijit/fitnessandwellnessbackend/DTO/UserRequestDto.java
@@ -0,0 +1,9 @@
+package com.avijit.fitnessandwellnessbackend.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRequestDto {
+}
---
+++ src/main/java/com/avijit/fitnessandwellnessbackend/Model/UserModel.java
@@ -0,0 +1,8 @@
+package com.avijit.fitnessandwellnessbackend.Model;
+
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class UserModel {
+}
